2479db7e1e0cea56bca3792e11449ad0608d5808
Vladimir Sumenko <3760097@gmail.com>
2021-01-22 20:38:37 +0300

    hw_04 all tests done.

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
 from . import views
 
 # у нас два варианта: вывод всех постов или по сообществу
 urlpatterns = [
     path("", views.index, name="index"),
+    path("new/", views.new_post, name="new_post"),
     path("group/<slug:slug>/", views.group_posts, name="group-slug"),
+    path("group/", views.show_groups, name="show_groups"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
 from .models import Post, Group
+from .forms import PostForm
 
 
 def index(request):
@@ -15,3 +18,32 @@ def group_posts(request, slug=None):
     # Получаем все посты принадлежащие slug через related_name
     posts = group.posts.all()[:12]
     return render(request, "group.html", {"group": group, "posts": posts})
+
+
+def show_groups(request):
+    """ Показывает страницу со списком всех сообществ """
+    groups = [
+                {"title": group.title, "slug": group.slug}
+                for group in Group.objects.all()
+             ]
+    # ссылка в корень
+    groups.insert(0, {"slug": "/", "title": "Посмотреть всё"})
+    return render(request, "show_groups.html", {"groups": groups})
+
+
+@login_required
+def new_post(request):
+    """  Создать новый пост"""
+    if request.POST:
+        form = PostForm(request.POST)
+        if form.is_valid():
+            # Пользователь хранится в request.username
+            # Данные формы после валидации лежат в form.cleaned_data['']
+            post = Post(author=request.user,
+                        text=form.cleaned_data['text'],
+                        group=form.cleaned_data['group'])
+            post.save()
+            return redirect(reverse_lazy("index"))
+    else:
+        form = PostForm()
+    return render(request, "new_post.html", {"form": form})
